203675573f6339bfee5d082a3c3b67e1e593cddb
joao pedro guerios <joaopedroguerios@gmail.com>
2025-08-13 01:15:50 -0300

    Configura deploy automatico Azure backend via GitHub Actions

---
+++ back-end/src/main/java/com/tcc/alzheimer/AlzheimerApplication.java
@@ -0,0 +1,13 @@
+package com.tcc.alzheimer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlzheimerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlzheimerApplication.class, args);
+	}
+
+}
