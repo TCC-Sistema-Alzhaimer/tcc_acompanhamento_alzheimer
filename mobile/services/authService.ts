07a1dd70192ed11a53d5bba8ef0bdd4c9f837d76
Luccachl <lucca.hmcps@gmail.com>
2025-09-15 23:19:34 -0300

    feat: Function de logout basica criada

--- mobile/services/authService.ts
+++ mobile/services/authService.ts
@@ -6,7 +6,8 @@ import {
   NetworkError,
   NotFoundError,
 } from "./errors";
-//TODO: Melhorar tratamento de erros e logica em geral
+import * as SecureStore from "expo-secure-store";
+
 export const login = async (email: string, password: string) => {
   try {
     const response = await api.post("auth/login", {
@@ -22,14 +23,12 @@ export const login = async (email: string, password: string) => {
         const message =
           error.response.data?.message || "Ocorreu um erro no servidor.";
 
-        // Lança erros específicos baseados no status HTTP
         if (status === 401 || status === 403) {
           throw new AuthenticationError(message);
         }
         if (status === 404) {
           throw new NotFoundError(message);
         }
-        // Para outros erros 4xx ou 5xx
         throw new ApiError(message);
       } else if (error.request) {
         throw new NetworkError(
@@ -38,5 +37,15 @@ export const login = async (email: string, password: string) => {
       }
     }
     throw new Error("Ocorreu um erro inesperado.");
+  }
+};
+
+export const logout = async () => {
+  try {
+    await SecureStore.deleteItemAsync("userToken");
+    console.log("Token removido com sucesso!");
+  } catch (error) {
+    console.error("Erro ao remover o token:", error);
+    throw new Error("Não foi possível remover a sessão do dispositivo.");
   }
 };
