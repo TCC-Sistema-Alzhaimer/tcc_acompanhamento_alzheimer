b6e1c545e287d0368883134073a3b9169f4e8b7d
Luccachl <lucca.hmcps@gmail.com>
2025-09-15 22:46:38 -0300

    feat: Autenticação implementada no mobile. Codigo ainda simples sem as complexidades necessarias

--- mobile/services/authService.ts
+++ mobile/services/authService.ts
@@ -1,31 +1,21 @@
-// services/authService.ts
+import { api } from "./api";
+//TODO: Melhorar tratamento de erros e logica em geral
+export const login = async (email: string, password: string) => {
+  try {
+    const response = await api.post("auth/login", {
+      email,
+      password,
+    });
+    console.log("Usuário autenticado:", response.data);
+    return response.data;
+  } catch (error: any) {
+    console.error(
+      "Falha na autenticação via API:",
+      error.response?.data || error.message
+    );
 
-/**
- * Simula uma chamada de API para autenticar um usuário.
- * Em um aplicativo real, aqui você faria uma requisição HTTP para o seu backend.
- *
- * @param email O email do usuário.
- * @param password A senha do usuário.
- * @returns Uma Promise que resolve com um token de autenticação em caso de sucesso.
- * @throws Uma Promise que rejeita com uma mensagem de erro em caso de falha.
- */
-export const login = (email: string, password: string) => {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      // Lógica de validação simulada
-      if (email === "joao.silva123@mail.com" && password === "123456") {
-        console.log("Usuário autenticado com sucesso!");
-        resolve({
-          token: "fake-jwt-token-for-joao-silva",
-          user: {
-            name: "João da Silva",
-            email: "joao.silva123@mail.com",
-          },
-        });
-      } else {
-        console.error("Falha na autenticação: Credenciais inválidas.");
-        reject(new Error("E-mail ou senha inválidos."));
-      }
-    }, 1500); // Simula 1.5 segundos de delay da rede
-  });
+    const errorMessage =
+      error.response?.data?.message || "E-mail ou senha inválidos.";
+    throw new Error(errorMessage);
+  }
 };
