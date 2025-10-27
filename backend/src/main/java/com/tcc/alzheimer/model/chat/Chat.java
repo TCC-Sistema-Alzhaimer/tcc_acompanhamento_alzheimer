2a3ee170acb6019aff4c70de0f3d5bdfc5e58ce9
Wesley Parastchuk <wesleyparastchuk@gmail.com>
2025-10-27 16:42:07 -0300

    feat: chat models

---
+++ backend/src/main/java/com/tcc/alzheimer/model/chat/Chat.java
@@ -0,0 +1,82 @@
+package com.tcc.alzheimer.model.chat;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity
+@Table(name = "chat")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Chat {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", nullable = false, length = 120)
+    private String name;
+
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
+
+    @OneToMany(mappedBy = "chat", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
+    @EqualsAndHashCode.Exclude
+    @ToString.Exclude
+    @Builder.Default
+    private List<ChatMessage> messages = new ArrayList<>();
+
+    @OneToMany(mappedBy = "chat", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
+    @EqualsAndHashCode.Exclude
+    @ToString.Exclude
+    @Builder.Default
+    private Set<ChatUser> participants = new HashSet<>();
+
+    @PrePersist
+    public void prePersist() {
+        if (createdAt == null) {
+            createdAt = LocalDateTime.now();
+        }
+    }
+
+    public void addParticipant(ChatUser participant) {
+        participants.add(participant);
+        participant.setChat(this);
+    }
+
+    public void removeParticipant(ChatUser participant) {
+        participants.remove(participant);
+        participant.setChat(null);
+    }
+
+    public void addMessage(ChatMessage message) {
+        messages.add(message);
+        message.setChat(this);
+    }
+
+    public void removeMessage(ChatMessage message) {
+        messages.remove(message);
+        message.setChat(null);
+    }
+}
---
+++ backend/src/main/java/com/tcc/alzheimer/model/chat/ChatUser.java
@@ -0,0 +1,46 @@
+package com.tcc.alzheimer.model.chat;
+
+import com.tcc.alzheimer.model.roles.User;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity
+@Table(name = "chat_user", uniqueConstraints = {
+        @UniqueConstraint(name = "uk_chat_user_unique", columnNames = { "chat_id", "user_id" }) })
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ChatUser {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "chat_id", referencedColumnName = "id", nullable = false)
+    @EqualsAndHashCode.Exclude
+    @ToString.Exclude
+    private Chat chat;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "user_id", referencedColumnName = "id", nullable = false)
+    @EqualsAndHashCode.Exclude
+    @ToString.Exclude
+    private User user;
+
+}
